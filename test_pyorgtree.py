1195971ac37e62b8456e06052dde5a8805ed5ecd
Andrei Matveyeu <andrei@ideabulbs.com>
2013-08-02 22:33:59 +0200

    minimal unit tests added

--- test_pyorgtree.py
+++ test_pyorgtree.py
@@ -4,13 +4,20 @@ from pyorgtree import *
 class TestPyOrgTree(object):
     def test_01(self):
         tree = OrgTree('')
-        tree.read_from_file('tree.org', 0, 0)
-        print tree
-        print tree.get_data()
+        tree.read_from_file('test_data/tree01.org', 0, 0)
+        assert tree.get_children()[0].get_hash() == '12345'
+        
         tree_dict = tree.get_tree_dict()
+        assert '12345' in tree_dict.keys()
+        
         children = tree_dict['12345'].get_children()
-        for child in children:
-            print child.get_parent().get_hash()
-            print child.get_header()
-        print tree_dict['45678'].get_parent().get_hash()
-        assert 1 == 1
+        assert len(children) == 3
+
+        children = tree.get_children()
+        assert len(children) == 1
+
+        children = tree_dict['45678'].get_children()
+        assert len(children) == 0
+
+        assert tree_dict['45678'].get_parent().get_hash() == '23456'
+
