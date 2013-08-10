69f65eb9a2b8b753eca83e44cb0d2d15145faadd
Andrei Matveyeu <andrei@ideabulbs.com>
2013-08-10 13:47:38 +0200

    schedule class hierarchy created

--- schedule.py
+++ schedule.py
@@ -3,34 +3,11 @@ import datetime
 import os
 import cPickle
 
-class Schedule(object):
+class ScheduleBase(object):
 	schedule_line = None
-	has_repeater = None
-	datetime = None
-	has_dateonly = None
-	keyword = "SCHEDULED"
-	repeater = None
-	repeat_interval = None
-
+	keyword = ""
 	def __init__(self, schedule_line):
-		schedule_datetime_match = re.compile(".{0,}%s: <[0-9][0-9][0-9][0-9]-[0-1][0-9]-[0-3][0-9] [a-zA-Z]{3} [0-2][0-9]:[0-5][0-9].{0,}$" % self.keyword)
-		schedule_date_match = re.compile(".{0,}%s: <[0-9][0-9][0-9][0-9]-[0-1][0-9]-[0-3][0-9] [a-zA-Z]{3}.{0,}$" % self.keyword)
-		schedule_repeater_match = re.compile(".{1,} [\+]{1,2}[0-9]{1,4}[dwmy]")
-		schedule_delay_match = re.compile(".{1,} [\-]{1,2}[0-9]{1,4}[dwmy]")
-		self.schedule_line = schedule_line
-		if schedule_datetime_match.match(self.schedule_line):
-			self.datetime = self._extract_datetime(self.schedule_line)
-			self.has_dateonly = False
-		elif schedule_date_match.match(self.schedule_line):
-			self.datetime = self._extract_date(self.schedule_line)
-			self.has_dateonly = True
-		else:
-			raise Exception("Can't parse line: %s" % self.schedule_line)
-		if schedule_repeater_match.match(self.schedule_line):
-			self.repeater = self._extract_repeater(self.schedule_line)
-		if schedule_delay_match.match(self.schedule_line):
-			self.delay = self._extract_delay(self.schedule_line)
-
+		self.schedule_line = schedule_line		
 	def _extract_repeater(self, line):
 		repeater = re.sub(".{1,} (?P<repeater>[\+]{1,2}[0-9]{1,4}[dwmy]).{1,}", "\g<repeater>", line)
 		return repeater
@@ -54,16 +31,21 @@ class Schedule(object):
 		minute = int(time_string[14:16])
 		return datetime.datetime(year, month, day, hour, minute)
 
-	def has_date_only(self):
-		return self.has_dateonly
+class ScheduleRepeater(object):
+	repeater = -1
+	repeat_interval = None
 	def has_repeater(self):
+		if self.repeater == -1:
+			self.get_repeater()
 		return self.repeater != None
+		
 	def get_repeater(self):
+		if self.repeater == -1:
+			self.repeater = None
+			schedule_repeater_pattern = re.compile(".{1,} [\+]{1,2}[0-9]{1,4}[dwmy]")
+			if schedule_repeater_pattern.match(self.schedule_line):
+				self.repeater = self._extract_repeater(self.schedule_line)
 		return self.repeater
-	def has_delay(self):
-		return self.delay != None
-	def get_delay(self):
-		return self.delay
 	def has_overdue_repeater(self):
 		if self.has_repeater():
 			if re.compile("\+[0-9]").match(self.get_repeater()):
@@ -81,14 +63,53 @@ class Schedule(object):
 			return (interval, unit)
 		else:
 			return None
+
+class ScheduleDelay(object):
+	delay = -1
+	def has_delay(self):
+		if self.delay == -1:
+			self.get_delay()
+		return self.delay != None
+		
+	def get_delay(self):
+		if self.delay == -1:
+			schedule_delay_pattern = re.compile(".{1,} [\-]{1,2}[0-9]{1,4}[dwmy]")
+			self.delay = None
+			if schedule_delay_pattern.match(self.schedule_line):
+				self.delay = self._extract_delay(self.schedule_line)
+		return self.delay
+		
 	def get_delay_interval(self):
 		if self.has_delay():
 			interval = re.sub("[-]{1,2}(?P<num>[0-9]{1,4}).{1,}", "\g<num>", self.delay)
 			interval = int(interval)
 			unit = re.sub(".{1,}(?P<unit>[dwmy])", "\g<unit>", self.delay)
 		return (interval, unit)
+	
+class Schedule(ScheduleBase, ScheduleRepeater, ScheduleDelay):
+	datetime = -1
+	keyword = "SCHEDULED"
+	date_only = None
+	
+	def __init__(self, schedule_line):
+		super(Schedule, self).__init__(schedule_line)
 
+	def has_date_only(self):
+		if self.datetime == -1:
+			self.get_datetime()
+		return self.date_only
+		
 	def get_datetime(self):
+		if self.datetime == -1:
+			self.datetime = None
+			schedule_datetime_pattern = re.compile(".{0,}%s: <[0-9][0-9][0-9][0-9]-[0-1][0-9]-[0-3][0-9] [a-zA-Z]{3} [0-2][0-9]:[0-5][0-9].{0,}$" % self.keyword)
+			schedule_date_pattern = re.compile(".{0,}%s: <[0-9][0-9][0-9][0-9]-[0-1][0-9]-[0-3][0-9] [a-zA-Z]{3}.{0,}$" % self.keyword)
+			if schedule_datetime_pattern.match(self.schedule_line):
+				self.datetime = self._extract_datetime(self.schedule_line)
+				self.date_only = False
+			elif schedule_date_pattern.match(self.schedule_line):
+				self.datetime = self._extract_date(self.schedule_line)
+				self.date_only = True
 		return self.datetime
 
 class Deadline(Schedule):
